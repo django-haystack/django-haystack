437118c9d77266530d51e89330d691935beb2858
Daniel Lindsley <daniel@toastdriven.com>
2009-01-21 00:40:01 -0600

    Added autodiscover support.

--- djangosearch/__init__.py
+++ djangosearch/__init__.py
@@ -1,2 +1,40 @@
 from djangosearch.indexes import ModelIndex
 from djangosearch.sites import IndexSite
+
+
+def autodiscover():
+    """
+    Automatically build the site index.
+    
+    Again, almost exactly as django.contrib.admin does things, for consistency.
+    """
+    import imp
+    from django.conf import settings
+
+    for app in settings.INSTALLED_APPS:
+        # For each app, we need to look for an indexes.py inside that app's
+        # package. We can't use os.path here -- recall that modules may be
+        # imported different ways (think zip files) -- so we need to get
+        # the app's __path__ and look for indexes.py on that path.
+
+        # Step 1: find out the app's __path__ Import errors here will (and
+        # should) bubble up, but a missing __path__ (which is legal, but weird)
+        # fails silently -- apps that do weird things with __path__ might
+        # need to roll their own index registration.
+        try:
+            app_path = __import__(app, {}, {}, [app.split('.')[-1]]).__path__
+        except AttributeError:
+            continue
+
+        # Step 2: use imp.find_module to find the app's indexes.py. For some
+        # reason imp.find_module raises ImportError if the app can't be found
+        # but doesn't actually try to import the module. So skip this app if
+        # its indexes.py doesn't exist
+        try:
+            imp.find_module('indexes', app_path)
+        except ImportError:
+            continue
+
+        # Step 3: import the app's admin file. If this has errors we want them
+        # to bubble up.
+        __import__("%s.indexes" % app)
