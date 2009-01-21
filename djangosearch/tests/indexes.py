71bd20550f48cdf70b08bfcd6a70581520516074
Daniel Lindsley <daniel@toastdriven.com>
2009-01-21 08:54:37 -0600

    Started adding tests.

---
+++ djangosearch/tests/indexes.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from djangosearch.indexes import ModelIndex
+
+
+class ModelIndexTestCase(TestCase):
+    pass
---
+++ djangosearch/tests/query.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from djangosearch.query import SearchQuerySet
+
+
+class SearchQuerySetTestCase(TestCase):
+    pass
