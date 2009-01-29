a0ab50c9546144d6a8d3c4f7e2a035681ccb7cd3
Daniel Lindsley <daniel@toastdriven.com>
2009-01-29 00:04:42 -0600

    Added more imports for future test coverage.

--- djangosearch/tests/__init__.py
+++ djangosearch/tests/__init__.py
@@ -1,2 +1,5 @@
+from djangosearch.tests.backends.solr import *
+from djangosearch.tests.indexes import *
 from djangosearch.tests.query import *
 from djangosearch.tests.sites import *
+from djangosearch.tests.views import *
