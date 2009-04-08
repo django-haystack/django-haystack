b3b771ff88829fc1962666085e58a68b2b2c80df
Daniel Lindsley <daniel@toastdriven.com>
2009-04-08 02:07:49 -0500

    Added setup.py. Thanks cyberdelia for prompting it.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils import setup
+
+setup(
+    name='django-haystack',
+    version='1.0.0b',
+    description='Pluggable search for Django.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='http://haystacksearch.org/',
+    package_dir={'haystack': 'haystack'},
+    packages=['haystack', 'haystack.backends', 'haystack.management', 'haystack.management.commands'],
+    package_data={'haystack': ['templates/*']},
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+)
