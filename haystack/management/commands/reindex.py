6cd251f0e85d10c7aa901ccf071ccf083167e9e0
Daniel Lindsley <daniel@toastdriven.com>
2009-06-30 15:48:24 -0500

    Django 1.0.X compatibility fix for the reindex command.

--- haystack/management/commands/reindex.py
+++ haystack/management/commands/reindex.py
@@ -11,6 +11,7 @@ DEFAULT_AGE = None
 
 
 class Command(AppCommand):
+    help = "Reindex the given app(s)."
     option_list = AppCommand.option_list + (
         make_option('-a', '--age', action='store', dest='age',
             default=DEFAULT_AGE, type='int',
@@ -25,7 +26,21 @@ class Command(AppCommand):
         #     help='Verbosity level; 0=minimal output, 1=normal output, 2=all output'
         # ),
     )
-    help = "Reindex the given app(s)."
+    
+    # Django 1.0.X compatibility.
+    verbosity_present = False
+    
+    for option in option_list:
+        if option.get_opt_string() == '--verbosity':
+            verbosity_present = True
+    
+    if verbosity_present is False:
+        option_list = option_list + (
+            make_option('--verbosity', action='store', dest='verbosity', default='1',
+                type='choice', choices=['0', '1', '2'],
+                help='Verbosity level; 0=minimal output, 1=normal output, 2=all output'
+            ),
+        )
 
     def handle(self, *apps, **options):
         self.verbosity = int(options.get('verbosity', 1))
