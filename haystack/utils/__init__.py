bbe65edb98fac9289234c8a6fe5b5b58c64cc15b
Daniel Lindsley <daniel@toastdriven.com>
2010-05-24 00:29:50 -0500

    Fixed a bug in the ``Highlighter`` which would double-highlight HTML tags. Thanks to EmilStenstrom for the original patch.

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -139,18 +139,47 @@ class Highlighter(object):
     
     def render_html(self, highlight_locations=None, start_offset=None, end_offset=None):
         # Start by chopping the block down to the proper window.
-        highlighted_chunk = self.text_block[start_offset:end_offset]
+        text = self.text_block[start_offset:end_offset]
         
-        for word in self.query_words:
-            for special_char in ('+', '*', '.', '?'):
-                word = word.replace(special_char, '\%s' % special_char)
+        # Invert highlight_locations to a location -> term list
+        term_list = []
+        
+        for term, locations in highlight_locations.items():
+            term_list += [(loc - start_offset, term) for loc in locations]
             
-            word_re = re.compile("(%s)" % word, re.I)
+        loc_to_term = sorted(term_list)
+        
+        # Prepare the highlight template
+        if self.css_class:
+            hl_start = '<%s class="%s">' % (self.html_tag, self.css_class)
+        else:
+            hl_start = '<%s>' % (self.html_tag)
+        
+        hl_end = '</%s>' % self.html_tag
+        highlight_length = len(hl_start + hl_end)
+        
+        # Copy the part from the start of the string to the first match,
+        # and there replace the match with a highlighted version.
+        highlighted_chunk = ""
+        matched_so_far = 0
+        prev = 0
+        prev_str = ""
+        
+        for cur, cur_str in loc_to_term:
+            # This can be in a different case than cur_str
+            actual_term = text[cur:cur + len(cur_str)]
             
-            if self.css_class:
-                highlighted_chunk = re.sub(word_re, r'<%s class="%s">\1</%s>' % (self.html_tag, self.css_class, self.html_tag), highlighted_chunk)
-            else:
-                highlighted_chunk = re.sub(word_re, r'<%s>\1</%s>' % (self.html_tag, self.html_tag), highlighted_chunk)
+            # Handle incorrect highlight_locations by first checking for the term
+            if actual_term.lower() == cur_str:
+                highlighted_chunk += text[prev + len(prev_str):cur] + hl_start + actual_term + hl_end
+                prev = cur
+                prev_str = cur_str
+                
+                # Keep track of how far we've copied so far, for the last step
+                matched_so_far = cur + len(actual_term)
+        
+        # Don't forget the chunk after the last term
+        highlighted_chunk += text[matched_so_far:]
         
         if start_offset > 0:
             highlighted_chunk = '...%s' % highlighted_chunk
