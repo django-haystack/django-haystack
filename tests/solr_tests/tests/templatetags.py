299e3aef8193e3b0323febc948b248a51b8eb3c3
Travis Cline <travis.cline@gmail.com>
2010-10-25 16:39:11 -0500

    Changed mlt test to use a set comparison to eliminate failures due to ordering differences.

--- tests/solr_tests/tests/templatetags.py
+++ tests/solr_tests/tests/templatetags.py
@@ -6,6 +6,10 @@ from haystack.indexes import *
 from haystack.backends.solr_backend import SearchBackend
 from haystack.sites import SearchSite
 from core.models import MockModel
+try:
+    set
+except NameError:
+    from sets import Set as set
 
 
 class MLTSearchIndex(RealTimeSearchIndex):
@@ -52,7 +56,7 @@ class MoreLikeThisTagTestCase(TestCase):
         context = {
             'entry': mock,
         }
-        self.assertEqual(self.render(template, context), u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.23 core.mockmodel.15 core.mockmodel.21 ')
+        self.assertEqual(set(self.render(template, context).split()), set(u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.23 core.mockmodel.15 core.mockmodel.21 '.split()))
     
     def test_more_like_this_without_limit(self):
         mock = MockModel.objects.get(pk=3)
@@ -60,4 +64,4 @@ class MoreLikeThisTagTestCase(TestCase):
         context = {
             'entry': mock,
         }
-        self.assertEqual(self.render(template, context), u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.23 core.mockmodel.15 core.mockmodel.21 core.mockmodel.13 core.mockmodel.17 core.mockmodel.16 core.mockmodel.20 core.mockmodel.1 core.mockmodel.22 core.mockmodel.19 core.mockmodel.8 core.mockmodel.6 core.mockmodel.4 core.mockmodel.11 core.mockmodel.14 core.mockmodel.12 core.mockmodel.9 core.mockmodel.7 core.mockmodel.10 core.mockmodel.5 ')
+        self.assertEqual(set(self.render(template, context).split()), set(u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.23 core.mockmodel.15 core.mockmodel.21 core.mockmodel.13 core.mockmodel.17 core.mockmodel.16 core.mockmodel.20 core.mockmodel.1 core.mockmodel.22 core.mockmodel.19 core.mockmodel.8 core.mockmodel.6 core.mockmodel.4 core.mockmodel.11 core.mockmodel.14 core.mockmodel.12 core.mockmodel.9 core.mockmodel.7 core.mockmodel.10 core.mockmodel.5'.split()))
