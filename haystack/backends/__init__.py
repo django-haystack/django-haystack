63dffe22e9cbb28c1de3ab73fb70aa91b1394d02
oyiptong <oyiptong@zompf.com>
2010-10-29 20:10:38 -0400

    Fixes SearchQuerySet not pickleable. Closes #278

--- haystack/backends/__init__.py
+++ haystack/backends/__init__.py
@@ -299,7 +299,7 @@ class BaseSearchQuery(object):
         del(obj_dict['backend'])
         # Rip off the class bits as we'll be using this path when we go to load
         # the backend.
-        obj_dict['backend_used'] = ".".join(str(self.backend).replace("<class '", "").replace("'>", "").split(".")[0:-1])
+        obj_dict['backend_used'] = ".".join(str(self.backend).replace("<", "").split(".")[0:-1])
         return obj_dict
     
     def __setstate__(self, obj_dict):
