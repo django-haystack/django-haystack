7ab27f258e412e3c0d23b7355049795927a06ac2
David Cramer <dcramer@gmail.com>
2011-03-29 11:40:31 -0700

    Add --site argument to build_solr_schema

--- haystack/management/commands/build_solr_schema.py
+++ haystack/management/commands/build_solr_schema.py
@@ -3,18 +3,30 @@ import sys
 from django.core.management.base import BaseCommand
 from django.template import loader, Context
 from haystack.constants import ID, DJANGO_CT, DJANGO_ID, DEFAULT_OPERATOR
-
+try:
+    from django.utils import importlib
+except ImportError:
+    from haystack.utils import importlib
 
 class Command(BaseCommand):
     help = "Generates a Solr schema that reflects the indexes."
     base_options = (
         make_option("-f", "--filename", action="store", type="string", dest="filename",
                     help='If provided, directs output to a file instead of stdout.'),
+        make_option("-s", "--site", action="store", type="string", dest="site",
+                    help='If provided, configures Haystack to use the appropriate site module. (Defaults to `haystack.site`)'),
     )
     option_list = BaseCommand.option_list + base_options
     
     def handle(self, **options):
         """Generates a Solr schema that reflects the indexes."""
+        if options.get('site'):
+            mod_name, attr_name = options['site'].rsplit('.', 1)
+            self.site = getattr(importlib.import_module(mod_name), attr_name)
+        else:
+            from haystack import site
+            self.site = site
+
         schema_xml = self.build_template()
         
         if options.get('filename'):
@@ -24,8 +36,7 @@ class Command(BaseCommand):
     
     def build_context(self):
         # Cause the default site to load.
-        from haystack import backend, site
-        content_field_name, fields = backend.SearchBackend().build_schema(site.all_searchfields())
+        content_field_name, fields = self.site.backend.build_schema(self.site.all_searchfields())
         return Context({
             'content_field_name': content_field_name,
             'fields': fields,
