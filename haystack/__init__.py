0f9e75238c47ee0b64c0924b9f45bea4943b528f
Daniel Lindsley <daniel@toastdriven.com>
2011-04-12 13:21:54 -0500

    Revert "Shifted ``handle_registrations`` into ``models.py``."
    This seems to be breaking for people, despite working here & passing tests. Back to the drawing board...
    
    This reverts commit 106758f88a9bc5ab7e505be62d385d876fbc52fe.

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -1,3 +1,4 @@
+import inspect
 import logging
 import os
 from django.conf import settings
@@ -11,6 +12,7 @@ except ImportError:
 
 __author__ = 'Daniel Lindsley'
 __version__ = (1, 2, 0, 'beta')
+__all__ = ['backend']
 
 
 # Setup default logging.
@@ -24,44 +26,6 @@ if not hasattr(settings, "HAYSTACK_SITECONF"):
     raise ImproperlyConfigured("You must define the HAYSTACK_SITECONF setting before using the search framework.")
 if not hasattr(settings, "HAYSTACK_SEARCH_ENGINE"):
     raise ImproperlyConfigured("You must define the HAYSTACK_SEARCH_ENGINE setting before using the search framework.")
-
-
-def autodiscover():
-    """
-    Automatically build the site index.
-    
-    Again, almost exactly as django.contrib.admin does things, for consistency.
-    """
-    import imp
-    from django.conf import settings
-    
-    for app in settings.INSTALLED_APPS:
-        # For each app, we need to look for an search_indexes.py inside that app's
-        # package. We can't use os.path here -- recall that modules may be
-        # imported different ways (think zip files) -- so we need to get
-        # the app's __path__ and look for search_indexes.py on that path.
-        
-        # Step 1: find out the app's __path__ Import errors here will (and
-        # should) bubble up, but a missing __path__ (which is legal, but weird)
-        # fails silently -- apps that do weird things with __path__ might
-        # need to roll their own index registration.
-        try:
-            app_path = importlib.import_module(app).__path__
-        except AttributeError:
-            continue
-        
-        # Step 2: use imp.find_module to find the app's search_indexes.py. For some
-        # reason imp.find_module raises ImportError if the app can't be found
-        # but doesn't actually try to import the module. So skip this app if
-        # its search_indexes.py doesn't exist
-        try:
-            imp.find_module('search_indexes', app_path)
-        except ImportError:
-            continue
-        
-        # Step 3: import the app's search_index file. If this has errors we want them
-        # to bubble up.
-        importlib.import_module("%s.search_indexes" % app)
 
 
 # Load the search backend.
@@ -117,3 +81,74 @@ def load_backend(backend_name=None):
 
 
 backend = load_backend(settings.HAYSTACK_SEARCH_ENGINE)
+
+
+def autodiscover():
+    """
+    Automatically build the site index.
+    
+    Again, almost exactly as django.contrib.admin does things, for consistency.
+    """
+    import imp
+    from django.conf import settings
+    
+    for app in settings.INSTALLED_APPS:
+        # For each app, we need to look for an search_indexes.py inside that app's
+        # package. We can't use os.path here -- recall that modules may be
+        # imported different ways (think zip files) -- so we need to get
+        # the app's __path__ and look for search_indexes.py on that path.
+        
+        # Step 1: find out the app's __path__ Import errors here will (and
+        # should) bubble up, but a missing __path__ (which is legal, but weird)
+        # fails silently -- apps that do weird things with __path__ might
+        # need to roll their own index registration.
+        try:
+            app_path = importlib.import_module(app).__path__
+        except AttributeError:
+            continue
+        
+        # Step 2: use imp.find_module to find the app's search_indexes.py. For some
+        # reason imp.find_module raises ImportError if the app can't be found
+        # but doesn't actually try to import the module. So skip this app if
+        # its search_indexes.py doesn't exist
+        try:
+            imp.find_module('search_indexes', app_path)
+        except ImportError:
+            continue
+        
+        # Step 3: import the app's search_index file. If this has errors we want them
+        # to bubble up.
+        importlib.import_module("%s.search_indexes" % app)
+
+
+def handle_registrations(*args, **kwargs):
+    """
+    Ensures that any configuration of the SearchSite(s) are handled when
+    importing Haystack.
+    
+    This makes it possible for scripts/management commands that affect models
+    but know nothing of Haystack to keep the index up to date.
+    """
+    if not getattr(settings, 'HAYSTACK_ENABLE_REGISTRATIONS', True):
+        # If the user really wants to disable this, they can, possibly at their
+        # own expense. This is generally only required in cases where other
+        # apps generate import errors and requires extra work on the user's
+        # part to make things work.
+        return
+    
+    # This is a little dirty but we need to run the code that follows only
+    # once, no matter how many times the main Haystack module is imported.
+    # We'll look through the stack to see if we appear anywhere and simply
+    # return if we do, allowing the original call to finish.
+    stack = inspect.stack()
+    
+    for stack_info in stack[1:]:
+        if 'handle_registrations' in stack_info[3]:
+            return
+    
+    # Pull in the config file, causing any SearchSite initialization code to
+    # execute.
+    search_sites_conf = importlib.import_module(settings.HAYSTACK_SITECONF)
+
+
+handle_registrations()
--- haystack/models.py
+++ haystack/models.py
@@ -1,15 +1,9 @@
 # "Hey, Django! Look at me, I'm an app! For Serious!"
-import inspect
 import logging
-from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils.encoding import force_unicode
 from django.utils.text import capfirst
-try:
-    from django.utils import importlib
-except ImportError:
-    from haystack.utils import importlib
 
 
 # Not a Django model, but tightly tied to them and there doesn't seem to be a
@@ -169,36 +163,3 @@ class SearchResult(object):
         """
         self.__dict__.update(d)
         self.log = self._get_log()
-
-
-def handle_registrations(*args, **kwargs):
-    """
-    Ensures that any configuration of the SearchSite(s) are handled when
-    importing Haystack.
-    
-    This makes it possible for scripts/management commands that affect models
-    but know nothing of Haystack to keep the index up to date.
-    """
-    if not getattr(settings, 'HAYSTACK_ENABLE_REGISTRATIONS', True):
-        # If the user really wants to disable this, they can, possibly at their
-        # own expense. This is generally only required in cases where other
-        # apps generate import errors and requires extra work on the user's
-        # part to make things work.
-        return
-    
-    # This is a little dirty but we need to run the code that follows only
-    # once, no matter how many times the main Haystack module is imported.
-    # We'll look through the stack to see if we appear anywhere and simply
-    # return if we do, allowing the original call to finish.
-    stack = inspect.stack()
-    
-    for stack_info in stack[1:]:
-        if 'handle_registrations' in stack_info[3] or 'load_backend' in stack_info[3]:
-            return
-    
-    # Pull in the config file, causing any SearchSite initialization code to
-    # execute.
-    search_sites_conf = importlib.import_module(settings.HAYSTACK_SITECONF)
-
-
-handle_registrations()
