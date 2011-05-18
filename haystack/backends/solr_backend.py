122702bffc8073ba9e6eb30c44e84bbbca56ae7d
jhh <jhh@endgames.us>
2011-05-18 13:38:00 -0400

    adds facet.mincount, facet.limit, and facet.prefix

--- haystack/backends/solr_backend.py
+++ haystack/backends/solr_backend.py
@@ -99,7 +99,7 @@ class SearchBackend(BaseSearchBackend):
     @log_query
     def search(self, query_string, sort_by=None, start_offset=0, end_offset=None,
                fields='', highlight=False, facets=None, date_facets=None, query_facets=None,
-               narrow_queries=None, spelling_query=None,
+               narrow_queries=None, spelling_query=None, facet_mincount=None, facet_limit=None, facet_prefix=None,
                limit_to_registered_models=None, result_class=None, **kwargs):
         if len(query_string) == 0:
             return {
@@ -138,6 +138,18 @@ class SearchBackend(BaseSearchBackend):
         if facets is not None:
             kwargs['facet'] = 'on'
             kwargs['facet.field'] = facets
+            
+        if facet_mincount is not None:
+            kwargs['facet'] = 'on'
+            kwargs['facet.mincount'] = facet_mincount
+            
+        if facet_limit is not None:
+            kwargs['facet'] = 'on'
+            kwargs['facet.limit'] = facet_limit
+        
+        if facet_prefix is not None:
+            kwargs['facet'] = 'on'
+            kwargs['facet.prefix'] = facet_prefix
         
         if date_facets is not None:
             kwargs['facet'] = 'on'
@@ -454,6 +466,15 @@ class SearchQuery(BaseSearchQuery):
         
         if self.date_facets:
             kwargs['date_facets'] = self.date_facets
+            
+        if self.facet_mincount:
+            kwargs['facet_mincount'] = self.facet_mincount
+        
+        if self.facet_limit:
+            kwargs['facet_limit'] = self.facet_limit
+        
+        if self.facet_prefix:
+            kwargs['facet_prefix'] = self.facet_prefix
         
         if self.query_facets:
             kwargs['query_facets'] = self.query_facets
