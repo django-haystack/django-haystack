842cb675cc4d154b71786bcb3616138cf4f17cf4
Daniel Lindsley <daniel@toastdriven.com>
2011-08-18 09:39:02 -0500

    Fixed a bug with Whoosh's edge n-gram support to be consistent with the implementation in the other engines.

--- haystack/backends/whoosh_backend.py
+++ haystack/backends/whoosh_backend.py
@@ -148,7 +148,7 @@ class WhooshSearchBackend(BaseSearchBackend):
             elif field_class.field_type == 'ngram':
                 schema_fields[field_class.index_fieldname] = NGRAM(minsize=3, maxsize=15, stored=field_class.stored, field_boost=field_class.boost)
             elif field_class.field_type == 'edge_ngram':
-                schema_fields[field_class.index_fieldname] = NGRAMWORDS(minsize=2, maxsize=15, stored=field_class.stored, field_boost=field_class.boost)
+                schema_fields[field_class.index_fieldname] = NGRAMWORDS(minsize=2, maxsize=15, at='start', stored=field_class.stored, field_boost=field_class.boost)
             else:
                 schema_fields[field_class.index_fieldname] = TEXT(stored=True, analyzer=StemmingAnalyzer(), field_boost=field_class.boost)
 
