72b595b4e421edc5a443b627cffbb9309254c1f9
Aram Dulyan <aram00@gmail.com>
2011-10-05 14:11:14 +1100

    Fixed compatibility with Django pre-1.4 trunk. * The MAX_SHOW_ALL_ALLOWED variable is no longer available, and hence causes an ImportError with Django versions higher 1.3. * The "list_max_show_all" attribute on the ChangeList object is used instead. * This patch maintains compatibility with Django 1.3 and lower by trying to import the MAX_SHOW_ALL_ALLOWED variable first.

--- haystack/admin.py
+++ haystack/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.views.main import ChangeList, SEARCH_VAR
-from django.core.exceptions import PermissionDenied, ImproperlyConfigured
+from django.core.exceptions import PermissionDenied
 from django.core.paginator import Paginator, InvalidPage
 from django.shortcuts import render_to_response
 from django import template
@@ -12,13 +12,13 @@ try:
     from django.contrib.admin.options import csrf_protect_m
 except ImportError:
     from haystack.utils.decorators import method_decorator
-    
+
     # Do nothing on Django 1.1 and below.
     def csrf_protect(view):
         def wraps(request, *args, **kwargs):
             return view(request, *args, **kwargs)
         return wraps
-    
+
     csrf_protect_m = method_decorator(csrf_protect)
 
 def list_max_show_all(changelist):
@@ -39,18 +39,18 @@ class SearchChangeList(ChangeList):
     def get_results(self, request):
         if not SEARCH_VAR in request.GET:
             return super(SearchChangeList, self).get_results(request)
-        
+
         # Note that pagination is 0-based, not 1-based.
         sqs = SearchQuerySet().models(self.model).auto_query(request.GET[SEARCH_VAR]).load_all()
-        
+
         paginator = Paginator(sqs, self.list_per_page)
         # Get the number of objects, with admin filters applied.
         result_count = paginator.count
         full_result_count = SearchQuerySet().models(self.model).all().count()
-        
+
         can_show_all = result_count <= list_max_show_all(self)
         multi_page = result_count > self.list_per_page
-        
+
         # Get the list of objects to display on this page.
         try:
             result_list = paginator.page(self.page_num+1).object_list
@@ -59,7 +59,7 @@ class SearchChangeList(ChangeList):
             result_list = [result.object for result in result_list]
         except InvalidPage:
             result_list = ()
-        
+
         self.result_count = result_count
         self.full_result_count = full_result_count
         self.result_list = result_list
@@ -73,26 +73,26 @@ class SearchModelAdmin(ModelAdmin):
     def changelist_view(self, request, extra_context=None):
         if not self.has_change_permission(request, None):
             raise PermissionDenied
-        
+
         if not SEARCH_VAR in request.GET:
             # Do the usual song and dance.
             return super(SearchModelAdmin, self).changelist_view(request, extra_context)
-        
+
         # Do a search of just this model and populate a Changelist with the
         # returned bits.
         if not self.model in site.get_indexed_models():
             # Oops. That model isn't being indexed. Return the usual
             # behavior instead.
             return super(SearchModelAdmin, self).changelist_view(request, extra_context)
-        
+
         # So. Much. Boilerplate.
         # Why copy-paste a few lines when you can copy-paste TONS of lines?
         list_display = list(self.list_display)
-        
+
         changelist = SearchChangeList(request, self.model, list_display, self.list_display_links, self.list_filter, self.date_hierarchy, self.search_fields, self.list_select_related, self.list_per_page, self.list_editable, self)
         formset = changelist.formset = None
         media = self.media
-        
+
         # Build the action form and populate it with available actions.
         # Check actions to see if any are available on this changelist
         actions = self.get_actions(request)
@@ -101,12 +101,12 @@ class SearchModelAdmin(ModelAdmin):
             action_form.fields['action'].choices = self.get_action_choices(request)
         else:
             action_form = None
-        
+
         selection_note = ungettext('0 of %(count)d selected',
             'of %(count)d selected', len(changelist.result_list))
         selection_note_all = ungettext('%(total_count)s selected',
             'All %(total_count)s selected', changelist.result_count)
-        
+
         context = {
             'module_name': force_unicode(self.model._meta.verbose_name_plural),
             'selection_note': selection_note % {'count': len(changelist.result_list)},
@@ -130,4 +130,4 @@ class SearchModelAdmin(ModelAdmin):
             'admin/%s/change_list.html' % self.model._meta.app_label,
             'admin/change_list.html'
         ], context, context_instance=context_instance)
-            
+
