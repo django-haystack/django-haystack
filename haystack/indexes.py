af164d3b5619674f75277ce3de4fe6cafda640da
German M. Bravo <german.mb@deipi.com>
2011-10-07 14:10:00 -0700

    Added 'models' hint to

--- haystack/indexes.py
+++ haystack/indexes.py
@@ -191,7 +191,11 @@ class SearchIndex(object):
 
     def _get_backend(self, using):
         if using is None:
-            using = connection_router.for_write(index=self)
+            hints = {
+                'index': self,
+                'models': [self.get_model()]
+            }
+            using = connection_router.for_write(**hints)
 
         return connections[using].get_backend()
 
