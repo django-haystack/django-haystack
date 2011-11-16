aae473d9b782d910e2b371c6738ef6abd18bd5a4
Thom Linton <thom.linton@gmail.com>
2011-11-15 17:50:13 -0800

    __len__ may not return a value less than zero.

--- haystack/query.py
+++ haystack/query.py
@@ -87,7 +87,7 @@ class SearchQuerySet(object):
                 self._result_count = 0
 
         # This needs to return the actual number of hits, not what's in the cache.
-        return self._result_count - self._ignored_result_count
+        return max(self._result_count - self._ignored_result_count, 0)
 
     def __iter__(self):
         if self._cache_is_full():
@@ -114,7 +114,7 @@ class SearchQuerySet(object):
         if not self.query.has_run():
             return False
 
-        if len(self) <= 0:
+        if len(self) == 0:
             return True
 
         try:
