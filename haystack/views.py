4b087be5c22f0be08dc13a79b5734db508f470c0
Chris Adams <chris@improbable.org>
2011-12-06 10:34:51 -0500

    Avoid behaviour change by raising Http404 for invalid page numbers
    An earlier version of this branch would always display the first page in this
    case. For compatibility we now simply return 404.

--- haystack/views.py
+++ haystack/views.py
@@ -99,7 +99,7 @@ class SearchView(object):
         try:
             page_no = int(self.request.GET.get('page', 1))
         except (TypeError, ValueError):
-            page_no = 1
+            raise Http404
 
         start_offset = (page_no - 1) * self.results_per_page
         self.results[start_offset:start_offset + self.results_per_page]
