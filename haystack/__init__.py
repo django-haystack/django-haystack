be0d802e39eb2f2c43289903fb08a0fd30868cf8
Daniel Lindsley <daniel@toastdriven.com>
2011-12-09 01:31:44 -0600

    Bumped to v1.2.6!

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -11,7 +11,7 @@ except ImportError:
 
 
 __author__ = 'Daniel Lindsley'
-__version__ = (1, 2, 6, 'beta')
+__version__ = (1, 2, 6)
 __all__ = ['backend']
 
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='django-haystack',
-    version='1.2.6-beta',
+    version='1.2.6',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
