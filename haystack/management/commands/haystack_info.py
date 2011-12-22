0f2f284d970de1748cd0b84597bed27b239f80d0
Daniel Lindsley <daniel@toastdriven.com>
2011-12-21 19:15:42 -0600

    Better excluded index detection.

--- haystack/management/commands/haystack_info.py
+++ haystack/management/commands/haystack_info.py
@@ -3,14 +3,15 @@ from django.core.management.base import NoArgsCommand
 
 class Command(NoArgsCommand):
     help = "Provides feedback about the current Haystack setup."
-    
+
     def handle_noargs(self, **options):
         """Provides feedback about the current Haystack setup."""
         from haystack import connections
-        
-        indexed = connections['default'].get_unified_index().get_indexed_models()
+
+        unified_index = connections['default'].get_unified_index()
+        indexed = unified_index.get_indexed_models()
         index_count = len(indexed)
         print "Number of handled %s index(es)." % index_count
-        
+
         for index in indexed:
-            print "  - %s" % index
+            print "  - Model: %s by Index: %s" % (index.__name__, unified_index.indexes[index])
