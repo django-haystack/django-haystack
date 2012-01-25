0570a068d736b684c8d0333e31e280311cd419c9
Sheldon <sheldon.els@gmail.com>
2012-01-25 19:43:28 +0000

    added stage hunk command
    Conflicts:
    
    	Default.sublime-commands
    	Main.sublime-menu
    	git.py

--- git.py
+++ git.py
@@ -657,3 +657,43 @@ class GitCustomCommand(GitTextCommand):
         command_splitted = ['git'] + shlex.split(command)
         print command_splitted
         self.run_command(command_splitted)
+
+
+class GitAddSelectedHunkCommand(GitTextCommand):
+    def run(self, edit):
+        self.run_command(['git', 'diff', '--no-color', '-U0', self.get_file_name()], self.cull_diff)
+
+    def cull_diff(self, result):
+        selection = []
+        for sel in self.view.sel():
+            selection.append({
+                "start":self.view.rowcol(sel.begin())[0] + 1,
+                "end":self.view.rowcol(sel.end())[0] + 1
+            })
+        
+        hunks = [{"diff":""}];
+        i = 0;
+        matcher = re.compile('^@@ -([0-9]*)(?:,([0-9]*))? \+([0-9]*)(?:,([0-9]*))? @@')
+        for line in result.splitlines():
+            if line.startswith('@@'):
+                i += 1
+                match = matcher.match(line)
+                start = int(match.group(3))
+                end = match.group(4)
+                if(end): end = start + int(end)
+                else: end = start
+                hunks.append({"diff":"", "start":start, "end":end})
+            hunks[i]["diff"] += line + "\n"
+        
+        diffs = hunks[0]["diff"]
+        hunks.pop(0);
+        i = 0
+        for hunk in hunks:
+            for sel in selection:
+                if(sel["end"] < hunk["start"]): continue
+                if(sel["start"] > hunk["end"]): continue
+                diffs += hunk["diff"]# + "\n\nEND OF HUNK\n\n"
+                i += 1
+        
+        if(i): self.run_command(['git', 'apply', '--cached'], callback=self.panel, stdin=diffs)
+        else: sublime.status_message("No selected hunk")
