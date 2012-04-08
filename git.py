b373b013ba0b5c035c4ca8a97a5737a213b01ba5
Sheldon <sheldon.els@gmail.com>
2012-04-08 10:35:14 +0100

    no need for that to be commented out, the patch is correct in using tmp instead of the config var

--- git.py
+++ git.py
@@ -786,7 +786,6 @@ class GitAnnotateCommand(GitTextCommand):
     #    output is then parsed and regions are set accordingly.
     def run(self, view):
         # If the annotations are already running, we dont have to create a new tmpfile
-        # if self.active_view().settings().get('live_git_annotations'):
         if hasattr(view, "tmp"):
             self.compare_tmp(None)
             return
