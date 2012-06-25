414ccb8f885e19108c801999f114920160658c83
Jannis Leidel <jannis@leidel.info>
2012-06-25 22:32:15 +0200

    Moved away from Fabric again for now.

---
+++ tests/solr_tests/jetty-default.sh
@@ -0,0 +1,7 @@
+JAVA_HOME=/usr/java/default
+JAVA_OPTIONS="-Dsolr.solr.home=/usr/share/solr/example/solr $JAVA_OPTIONS"
+JETTY_HOME=/usr/share/solr/example
+JETTY_USER=solr
+JETTY_LOGS=/var/log/solr
+JAVA_HOME=/usr/lib/jvm/default-java
+JDK_DIRS="/usr/lib/jvm/default-java /usr/lib/jvm/java-6-sun"
