537a157e8ad94fa90331ca383df7d95d649c041d
Chris Adams <chris@improbable.org>
2012-09-30 13:13:08 -0400

    Tests: fix Solr MLT templatetag tests
    * Updated to account for test fixture drift
    * Added a random test record which should not be returned by Solr MLT queries
    * Switched to unittest2 addCleanup() to ensure that our monkey-patch is always
      called

--- tests/solr_tests/tests/templatetags.py
+++ tests/solr_tests/tests/templatetags.py
@@ -1,8 +1,11 @@
+from django.contrib.webdesign import lorem_ipsum
 from django.template import Template, Context
 from django.test import TestCase
+
 from haystack import connections
 from haystack import indexes
 from haystack.utils.loading import UnifiedIndex
+
 from core.models import MockModel
 from solr_tests.tests.solr_backend import clear_solr_index
 
@@ -29,13 +32,19 @@ class MoreLikeThisTagTestCase(TestCase):
         self.ui.build(indexes=[self.smmi])
         connections['default']._index = self.ui
 
+        def restore_globals():
+            connections['default']._index = self.old_ui
+
+        self.addCleanup(restore_globals)
+
         # Force indexing of the content.
         for mock in MockModel.objects.all():
             mock.save()
 
-    def tearDown(self):
-        connections['default']._index = self.old_ui
-        super(MoreLikeThisTagTestCase, self).tearDown()
+        # Create one test record which shouldn't match anything at all:
+        MockModel.objects.create(author="test_bogon",
+                                 foo=lorem_ipsum.paragraphs(3),
+                                 tag_id=1)
 
     def render(self, template, context):
         # Why on Earth does Django not have a TemplateTestCase yet?
@@ -49,7 +58,9 @@ class MoreLikeThisTagTestCase(TestCase):
         context = {
             'entry': mock,
         }
-        self.assertEqual(set(self.render(template, context).split()), set(u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.17 core.mockmodel.15 core.mockmodel.21 '.split()))
+
+        self.assertEqual(set(self.render(template, context).split()),
+                         set(u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.23 core.mockmodel.15 core.mockmodel.8'.split()))
 
     def test_more_like_this_without_limit(self):
         mock = MockModel.objects.get(pk=3)
@@ -57,4 +68,11 @@ class MoreLikeThisTagTestCase(TestCase):
         context = {
             'entry': mock,
         }
-        self.assertEqual(set(self.render(template, context).split()), set(u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.23 core.mockmodel.15 core.mockmodel.21 core.mockmodel.13 core.mockmodel.17 core.mockmodel.16 core.mockmodel.20 core.mockmodel.1 core.mockmodel.22 core.mockmodel.19 core.mockmodel.8 core.mockmodel.6 core.mockmodel.11 core.mockmodel.14 core.mockmodel.12 core.mockmodel.9 core.mockmodel.7'.split()))
+
+        # Our test objects are very similar and will match every other record:
+        mock_pks = MockModel.objects.exclude(pk=mock.pk).exclude(author="test_bogon")
+        mock_pks = mock_pks.values_list("pk", flat=True)
+
+        rendered_ids = set(self.render(template, context).split())
+
+        self.assertEqual(rendered_ids, set("core.mockmodel.%s" % i for i in mock_pks))
