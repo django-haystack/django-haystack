b332aff026ebfa02c15c04094bd309453530de49
Chris Adams <chris@improbable.org>
2012-09-30 22:02:04 -0400

    Convert MLT templatetag tests to rely on mocks
    This is a bit of a style change:
    
    Advantages:
    * Completely backend neutral
    * Very fast
    * Does not randomly break based on Solr's internal workings (e.g. run tests
      repeatedly to watch pass/fail cycles; remove data/ to be consistent)
    * Avoids hard-coding fixture keys
    
    Disadvantages:
    * Relies on backend MLT queryset tests for coverage
    * Mock syntax slightly obtuse
    
    Thoughts:
    * Should we simply call the template tag function directly?

--- tests/solr_tests/tests/templatetags.py
+++ tests/solr_tests/tests/templatetags.py
@@ -1,78 +1,55 @@
-from django.contrib.webdesign import lorem_ipsum
+# encoding: utf-8
+from mock import call, patch
+
 from django.template import Template, Context
 from django.test import TestCase
 
-from haystack import connections
-from haystack import indexes
-from haystack.utils.loading import UnifiedIndex
-
 from core.models import MockModel
-from solr_tests.tests.solr_backend import clear_solr_index
 
 
-class MLTSearchIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, model_attr='foo')
-
-    def get_model(self):
-        return MockModel
-
-
+@patch("haystack.templatetags.more_like_this.SearchQuerySet")
 class MoreLikeThisTagTestCase(TestCase):
-    fixtures = ['bulk_data.json']
-
-    def setUp(self):
-        super(MoreLikeThisTagTestCase, self).setUp()
-
-        clear_solr_index()
-
-        # Stow.
-        self.old_ui = connections['default'].get_unified_index()
-        self.ui = UnifiedIndex()
-        self.smmi = MLTSearchIndex()
-        self.ui.build(indexes=[self.smmi])
-        connections['default']._index = self.ui
-
-        def restore_globals():
-            connections['default']._index = self.old_ui
-
-        self.addCleanup(restore_globals)
-
-        # Force indexing of the content.
-        for mock in MockModel.objects.all():
-            mock.save()
-
-        # Create one test record which shouldn't match anything at all:
-        MockModel.objects.create(author="test_bogon",
-                                 foo=lorem_ipsum.paragraphs(3),
-                                 tag_id=1)
-
     def render(self, template, context):
         # Why on Earth does Django not have a TemplateTestCase yet?
         t = Template(template)
         c = Context(context)
         return t.render(c)
 
-    def test_more_like_this_with_limit(self):
-        mock = MockModel.objects.get(pk=3)
-        template = """{% load more_like_this %}{% more_like_this entry as related_content limit 5 %}{% for rc in related_content %}{{ rc.id }} {% endfor %}"""
-        context = {
-            'entry': mock,
-        }
+    def test_more_like_this_without_limit(self, mock_sqs):
+        mock_model = MockModel.objects.get(pk=3)
+        template = """{% load more_like_this %}{% more_like_this entry as related_content %}{% for rc in related_content %}{{ rc.id }}{% endfor %}"""
+        context = {'entry': mock_model}
 
-        self.assertEqual(set(self.render(template, context).split()),
-                         set(u'core.mockmodel.2 core.mockmodel.18 core.mockmodel.23 core.mockmodel.15 core.mockmodel.8'.split()))
+        mlt = mock_sqs.return_value.more_like_this
+        mlt.return_value = [{"id": "test_id"}]
 
-    def test_more_like_this_without_limit(self):
-        mock = MockModel.objects.get(pk=3)
-        template = """{% load more_like_this %}{% more_like_this entry as related_content %}{% for rc in related_content %}{{ rc.id }} {% endfor %}"""
-        context = {
-            'entry': mock,
-        }
+        self.assertEqual("test_id", self.render(template, context))
 
-        # Our test objects are very similar and will match every other record:
-        mock_pks = MockModel.objects.exclude(pk=mock.pk).exclude(author="test_bogon")
-        mock_pks = mock_pks.values_list("pk", flat=True)
+        mlt.assert_called_once_with(mock_model)
 
-        rendered_ids = set(self.render(template, context).split())
+    def test_more_like_this_with_limit(self, mock_sqs):
+        mock_model = MockModel.objects.get(pk=3)
+        template = """{% load more_like_this %}{% more_like_this entry as related_content limit 5 %}{% for rc in related_content %}{{ rc.id }}{% endfor %}"""
+        context = {'entry': mock_model}
 
-        self.assertEqual(rendered_ids, set("core.mockmodel.%s" % i for i in mock_pks))
+        mlt = mock_sqs.return_value.more_like_this
+        mlt.return_value.__getitem__.return_value = [{"id": "test_id"}]
+
+        self.assertEqual("test_id", self.render(template, context))
+
+        mlt.assert_called_once_with(mock_model)
+
+        mock_sqs.assert_has_calls([call().more_like_this(mock_model),
+                                   call().more_like_this().__getitem__(slice(None, 5))],
+                                   any_order=True)
+
+    def test_more_like_this_for_model(self, mock_sqs):
+        mock_model = MockModel.objects.get(pk=3)
+        template = """{% load more_like_this %}{% more_like_this entry as related_content for "core.mock" limit 5 %}{% for rc in related_content %}{{ rc.id }}{% endfor %}"""
+        context = {'entry': mock_model}
+
+        self.render(template, context)
+
+        mock_sqs.assert_has_calls([call().models().more_like_this(mock_model),
+                                   call().models().more_like_this().__getitem__(slice(None, 5))],
+                                   any_order=True)
