dae14cec50d31b66628ffa423e96721e1d8549f9
GovTrack.us <operations@govtrack.us>
2013-01-08 16:24:42 -0500

    fix prune_index to work with Xapian by using SearchQuerySet for iteration rather than the backend directly

--- haystack/management/commands/prune_index.py
+++ haystack/management/commands/prune_index.py
@@ -2,11 +2,11 @@ from optparse import make_option
 import sys
 from django.core.management.base import BaseCommand
 from haystack.constants import DEFAULT_ALIAS
-
+from pprint import pprint
 
 class Command(BaseCommand):
-    help = "Removes indexed documents that no longer correspond to objects in the Django ORM. Use '*' as the searchquery for Solr to select all documents. Otherwise you must construct your own search query appropriate for the backend."
-    args = 'searchquery'
+    help = "Removes indexed documents that no longer correspond to objects in the Django ORM."
+    
     base_options = (
         make_option('--noinput', action='store_false', dest='interactive', default=True,
             help='If provided, no prompts will be issued to the user and the data will be wiped out.'
@@ -23,23 +23,30 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         """Removes indexed documents that no longer correspond to objects in the Django ORM."""
+        
         from haystack import connections
+        from haystack.query import SearchQuerySet
+        
         verbosity = int(options.get('verbosity', 1))
         using = options.get('using')
         batch_size = int(options.get('batchsize'))
         
         backend = connections[using].get_backend()
+        qs = SearchQuerySet().using(using)
         
+        total = qs.count()
         start = 0
-        while True:
-            batch = backend.search(args[0], start_offset=start, end_offset=start+batch_size, fields='django_ct,django_id,score,id')
+        while start + batch_size <= total:
+            batch = qs[start:start+batch_size]
             
             # Get primary keys by model. Force the primary keys to integers
             # because we will need to compare them with the result of in_bulk
             # later. Construct as: model_pks[model_instance][pk] = backend_identifier
             model_pks = { }
-            for result in batch["results"]:
+            batch_objs = { }
+            for result in batch:
                 model_pks.setdefault(result.model, {})[int(result.pk)] = result.id
+                batch_objs[result.id] = result
                 
             # Take each model separately.
             for model, pks in model_pks.items():
@@ -49,7 +56,9 @@ class Command(BaseCommand):
                 # Check for missing pks from objs, indicating a missing object.
                 for pk, dotted_identifier in pks.items():
                     if pk not in objs:
-                        print dotted_identifier, "missing"
+                        print dotted_identifier, "missing:"
+                        pprint(batch_objs[dotted_identifier].get_stored_fields())
+                        print
 
                         # Without --noinput we ask to remove each one from the index.
                         if options.get('interactive', True):
@@ -60,12 +69,7 @@ class Command(BaseCommand):
                         # With --noinput, or the user chose yes, delete the record.
                         backend.remove(dotted_identifier)
             
-            # Know when we're done by comparing the last index requested to the total number
-            # of hits reported by the backend.
-            if batch['hits'] > 0:
-                print start, batch['hits'], str(100*start/batch['hits']) + "%"
-            if start + batch_size >= batch['hits']:
-                break
+            print start, total, str(100*start/total) + "%"
         
             start += batch_size
             
