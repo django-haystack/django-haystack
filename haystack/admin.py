4c29900fba2cd355ba3b83760e8d47a5835d90ce
Nate Przybyszewski <nathanp@mitre.org>
2013-02-21 14:49:07 -0500

    Updated SearchModelAdmin for Django 1.5 compatibility.
    Rather than copy-pasting boilerplate code from the Django ModelAdmin class, SearchModelAdmin now uses the get_changelist method to inject it's SearchChangeList class.

--- haystack/admin.py
+++ haystack/admin.py
@@ -1,25 +1,8 @@
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.views.main import ChangeList, SEARCH_VAR
-from django.core.exceptions import PermissionDenied
 from django.core.paginator import Paginator, InvalidPage
-from django.shortcuts import render_to_response
-from django import template
-from django.utils.encoding import force_unicode
-from django.utils.translation import ungettext
 from haystack import connections
 from haystack.query import SearchQuerySet
-try:
-    from django.contrib.admin.options import csrf_protect_m
-except ImportError:
-    from haystack.utils.decorators import method_decorator
-
-    # Do nothing on Django 1.1 and below.
-    def csrf_protect(view):
-        def wraps(request, *args, **kwargs):
-            return view(request, *args, **kwargs)
-        return wraps
-
-    csrf_protect_m = method_decorator(csrf_protect)
 
 def list_max_show_all(changelist):
     """
@@ -37,7 +20,7 @@ def list_max_show_all(changelist):
 
 class SearchChangeList(ChangeList):
     def get_results(self, request):
-        if not SEARCH_VAR in request.GET:
+        if not SEARCH_VAR in request.GET or request.GET[SEARCH_VAR] == "":
             return super(SearchChangeList, self).get_results(request)
 
         # Note that pagination is 0-based, not 1-based.
@@ -69,84 +52,8 @@ class SearchChangeList(ChangeList):
 
 
 class SearchModelAdmin(ModelAdmin):
-    @csrf_protect_m
-    def changelist_view(self, request, extra_context=None):
-        if not self.has_change_permission(request, None):
-            raise PermissionDenied
-
-        if not SEARCH_VAR in request.GET:
-            # Do the usual song and dance.
-            return super(SearchModelAdmin, self).changelist_view(request, extra_context)
-
-        # Do a search of just this model and populate a Changelist with the
-        # returned bits.
-        if not self.model in connections['default'].get_unified_index().get_indexed_models():
-            # Oops. That model isn't being indexed. Return the usual
-            # behavior instead.
-            return super(SearchModelAdmin, self).changelist_view(request, extra_context)
-
-        # So. Much. Boilerplate.
-        # Why copy-paste a few lines when you can copy-paste TONS of lines?
-        list_display = list(self.list_display)
-
-        kwargs = {
-            'request': request,
-            'model': self.model,
-            'list_display': list_display,
-            'list_display_links': self.list_display_links,
-            'list_filter': self.list_filter,
-            'date_hierarchy': self.date_hierarchy,
-            'search_fields': self.search_fields,
-            'list_select_related': self.list_select_related,
-            'list_per_page': self.list_per_page,
-            'list_editable': self.list_editable,
-            'model_admin': self
-        }
-
-        # Django 1.4 compatibility.
-        if hasattr(self, 'list_max_show_all'):
-            kwargs['list_max_show_all'] = self.list_max_show_all
-
-        changelist = SearchChangeList(**kwargs)
-        formset = changelist.formset = None
-        media = self.media
-
-        # Build the action form and populate it with available actions.
-        # Check actions to see if any are available on this changelist
-        actions = self.get_actions(request)
-        if actions:
-            action_form = self.action_form(auto_id=None)
-            action_form.fields['action'].choices = self.get_action_choices(request)
+    def get_changelist(self, request, **kwargs):
+        if self.model in connections['default'].get_unified_index().get_indexed_models():
+            return SearchChangeList
         else:
-            action_form = None
-
-        selection_note = ungettext('0 of %(count)d selected',
-            'of %(count)d selected', len(changelist.result_list))
-        selection_note_all = ungettext('%(total_count)s selected',
-            'All %(total_count)s selected', changelist.result_count)
-
-        context = {
-            'module_name': force_unicode(self.model._meta.verbose_name_plural),
-            'selection_note': selection_note % {'count': len(changelist.result_list)},
-            'selection_note_all': selection_note_all % {'total_count': changelist.result_count},
-            'title': changelist.title,
-            'is_popup': changelist.is_popup,
-            'cl': changelist,
-            'media': media,
-            'has_add_permission': self.has_add_permission(request),
-            # More Django 1.4 compatibility
-            'root_path': getattr(self.admin_site, 'root_path', None),
-            'app_label': self.model._meta.app_label,
-            'action_form': action_form,
-            'actions_on_top': self.actions_on_top,
-            'actions_on_bottom': self.actions_on_bottom,
-            'actions_selection_counter': getattr(self, 'actions_selection_counter', 0),
-        }
-        context.update(extra_context or {})
-        context_instance = template.RequestContext(request, current_app=self.admin_site.name)
-        return render_to_response(self.change_list_template or [
-            'admin/%s/%s/change_list.html' % (self.model._meta.app_label, self.model._meta.object_name.lower()),
-            'admin/%s/change_list.html' % self.model._meta.app_label,
-            'admin/change_list.html'
-        ], context, context_instance=context_instance)
-
+            return super(SearchModelAdmin, self).get_changelist(request, **kwargs)
