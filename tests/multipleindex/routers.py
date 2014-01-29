8d230554f694b54415539be993cec73f13524150
Chris Wilson <chris+github@aptivate.org>
2014-01-29 15:13:27 +0000

    Remove the broken and unused for_read in BazRouter

--- tests/multipleindex/routers.py
+++ tests/multipleindex/routers.py
@@ -4,11 +4,6 @@ from multipleindex.search_indexes import BazIndex
 
 class BazRouter(routers.BaseRouter):
     def for_read(self, models=None, **hints):
-        if models is None:
-            return None
-        import pdb; pdb.set_trace()
-        if Baz in models:
-            return 'filtered_whoosh'
         return None
 
     def for_write(self, index=None, instance=None, **hints):
