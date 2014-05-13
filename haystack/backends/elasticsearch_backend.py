3ccaddcc5df5b30f021e65aa53f04a6ee5d751b3
Pierre Dulac <dulacpier@gmail.com>
2014-05-13 22:04:31 +0200

    Use the `RequestsHttpConnection` connection class to support http auth

--- haystack/backends/elasticsearch_backend.py
+++ haystack/backends/elasticsearch_backend.py
@@ -97,7 +97,10 @@ class ElasticsearchSearchBackend(BaseSearchBackend):
         if not 'INDEX_NAME' in connection_options:
             raise ImproperlyConfigured("You must specify a 'INDEX_NAME' in your settings for connection '%s'." % connection_alias)
 
-        self.conn = elasticsearch.Elasticsearch(connection_options['URL'], timeout=self.timeout, **connection_options.get('KWARGS', {}))
+        self.conn = elasticsearch.Elasticsearch(connection_options['URL'],
+                                                connection_class=elasticsearch.RequestsHttpConnection,
+                                                timeout=self.timeout,
+                                                **connection_options.get('KWARGS', {}))
         self.index_name = connection_options['INDEX_NAME']
         self.log = logging.getLogger('haystack')
         self.setup_complete = False
