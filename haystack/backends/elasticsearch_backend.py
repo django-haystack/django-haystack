a04050ca50cc6bbc065e1088e029288f62d4a6b0
Albert Tugushev <albert@tugushev.ru>
2014-12-30 01:25:16 +0300

    Added source filtering for elasticsearch (see #1127)

--- haystack/backends/elasticsearch_backend.py
+++ haystack/backends/elasticsearch_backend.py
@@ -487,11 +487,16 @@ class ElasticsearchSearchBackend(BaseSearchBackend):
         if end_offset is not None and end_offset > start_offset:
             search_kwargs['size'] = end_offset - start_offset
 
+        _source = True
+        fields = kwargs.get('fields', [])
+        if fields:
+            _source = list(fields)
+
         try:
             raw_results = self.conn.search(body=search_kwargs,
                                            index=self.index_name,
                                            doc_type='modelresult',
-                                           _source=True)
+                                           _source=_source)
         except elasticsearch.TransportError as e:
             if not self.silently_fail:
                 raise
