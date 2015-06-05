edad3db083f83210a523352a139499bca2e56226
Chris Adams <chris@improbable.org>
2015-06-05 09:07:51 -0400

    v2.4.0 release candidate

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 setup(
     name='django-haystack',
-    version='2.4.0.dev1',
+    version='2.4.0rc1',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
