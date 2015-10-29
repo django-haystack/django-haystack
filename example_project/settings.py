93cc7e75f360b59b2c919ccfcdc4fc593abdd11e
Chris Adams <chris@improbable.org>
2015-10-29 09:59:12 -0400

    Minimal changes to the example project to allow test use

--- example_project/settings.py
+++ example_project/settings.py
@@ -4,13 +4,17 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 
+from django.conf import settings
+
+SECRET_KEY = 'CHANGE ME'
+
 # All the normal settings apply. What's included here are the bits you'll have
 # to customize.
 
 # Add Haystack to INSTALLED_APPS. You can do this by simply placing in your list.
-INSTALLED_APPS = INSTALLED_APPS + [
+INSTALLED_APPS = settings.INSTALLED_APPS + (
     'haystack',
-]
+)
 
 
 HAYSTACK_CONNECTIONS = {
@@ -20,6 +24,11 @@ HAYSTACK_CONNECTIONS = {
         'URL': 'http://localhost:9001/solr/example',
         'TIMEOUT': 60 * 5,
         'INCLUDE_SPELLING': True,
+    },
+    'elasticsearch': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://localhost:9200',
+        'INDEX_NAME': 'example_project'
     },
     'whoosh': {
         # For Whoosh:
@@ -31,9 +40,9 @@ HAYSTACK_CONNECTIONS = {
         # For Simple:
         'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
     },
-    'xapian': {
-        # For Xapian (requires the third-party install):
-        'ENGINE': 'xapian_backend.XapianEngine',
-        'PATH': os.path.join(os.path.dirname(__file__), 'xapian_index'),
-    }
+    # 'xapian': {
+    #     # For Xapian (requires the third-party install):
+    #     'ENGINE': 'xapian_backend.XapianEngine',
+    #     'PATH': os.path.join(os.path.dirname(__file__), 'xapian_index'),
+    # }
 }
