839a97ba8d3bb0ec16314e65b5f6adcc3d755e12
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2015-11-24 16:13:57 -0400

    Remove  as it'll be removed in Django 2.0

--- haystack/urls.py
+++ haystack/urls.py
@@ -1,16 +1,13 @@
 # encoding: utf-8
 
 from __future__ import absolute_import, division, print_function, unicode_literals
-
 from haystack.views import SearchView
 
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
 
-
-
-urlpatterns = patterns('haystack.views',
+urlpatterns = [
     url(r'^$', SearchView(), name='haystack_search'),
-)
+]
