266b8afc07bd829fdc6a65ddc3370eea4b62577b
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2015-11-24 17:42:57 -0400

    Remove unused import

--- haystack/fields.py
+++ haystack/fields.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import re
 
-from django.template import Context, loader
+from django.template import loader
 from django.utils import datetime_safe, six
 
 from haystack.exceptions import SearchFieldError
