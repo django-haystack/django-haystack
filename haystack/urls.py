86773c148e4c546ce2366b2313a1cf19fe1d90e6
Chris Adams <chris@improbable.org>
2016-03-25 10:47:59 -0400

    chg: dev: Update urls.py for supported Django versions
    * patterns() is deprecated
    * Removal of legacy Django imports
    
    Thanks to @claudep for the patch
    
    Closes #1327

--- haystack/urls.py
+++ haystack/urls.py
@@ -2,15 +2,10 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from django.conf.urls import url
+
 from haystack.views import SearchView
 
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
-
-
-
-urlpatterns = patterns('haystack.views',
+urlpatterns = [
     url(r'^$', SearchView(), name='haystack_search'),
-)
+]
--- test_haystack/core/urls.py
+++ test_haystack/core/urls.py
@@ -2,27 +2,26 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from haystack.forms import FacetedSearchForm
 from haystack.query import SearchQuerySet
-from haystack.views import FacetedSearchView, SearchView
+from haystack.views import FacetedSearchView, SearchView, basic_search
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
 
-
-urlpatterns += patterns('haystack.views',
     url(r'^$', SearchView(load_all=False), name='haystack_search'),
-    url(r'^faceted/$', FacetedSearchView(searchqueryset=SearchQuerySet().facet('author'), form_class=FacetedSearchForm), name='haystack_faceted_search'),
-    url(r'^basic/$', 'basic_search', {'load_all': False}, name='haystack_basic_search'),
-)
+    url(r'^faceted/$',
+        FacetedSearchView(searchqueryset=SearchQuerySet().facet('author'), form_class=FacetedSearchForm),
+        name='haystack_faceted_search'),
+    url(r'^basic/$', basic_search, {'load_all': False}, name='haystack_basic_search'),
+]
 
-urlpatterns += patterns('app-without-models',
+urlpatterns += [
     url(r'', include('test_haystack.test_app_without_models.urls', namespace='app-without-models')),
-)
+]
