c971855d8deb03eb8440729d599331d20a431c40
ahoho <alexander.hoyle@brattle.com>
2016-04-09 17:11:07 -0400

    Permit use of any Solr field param

--- haystack/fields.py
+++ haystack/fields.py
@@ -26,7 +26,7 @@ class SearchField(object):
     def __init__(self, model_attr=None, use_template=False, template_name=None,
                  document=False, indexed=True, stored=True, faceted=False,
                  default=NOT_PROVIDED, null=False, index_fieldname=None,
-                 facet_class=None, boost=1.0, weight=None, solr_explicit_field_type=None):
+                 facet_class=None, boost=1.0, weight=None, solr_field_params=None):
         # Track what the index thinks this field is called.
         self.instance_name = None
         self.model_attr = model_attr
@@ -42,17 +42,16 @@ class SearchField(object):
         self.boost = weight or boost
         self.is_multivalued = False
         
-        self.solr_explicit_field_type = solr_explicit_field_type
-        if self.solr_explicit_field_type:
+        if solr_field_params:
             from haystack.utils import loading
             from haystack.exceptions import MissingDependency
             try:
                 loading.load_backend('haystack.backends.solr_backend.SolrEngine')
             except MissingDependency:
-                raise NotImplementedError("""The solr_explicit_field_type attribute 
+                raise NotImplementedError("""The solr_field_params attribute 
                                            requires a configured Solr search backend.""")
             else:
-                self.field_type = self.solr_explicit_field_type
+                self.solr_field_params = self.solr_field_params
             
         # We supply the facet_class for making it easy to create a faceted
         # field based off of this field.
