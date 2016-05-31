b7d6c79eccebeb840b3e52a22b199a0236cba49b
Chris Adams <chris@improbable.org>
2016-05-31 15:39:18 -0400

    Removed deprecated use of Template.render() !minor
    Thanks to @PabloCastellano for the patch
    
    Closes #1372

--- haystack/fields.py
+++ haystack/fields.py
@@ -176,7 +176,7 @@ class SearchField(object):
             template_names = ['search/indexes/%s/%s_%s.txt' % (app_label, model_name, self.instance_name)]
 
         t = loader.select_template(template_names)
-        return t.render(Context({'object': obj}).flatten())
+        return t.render({'object': obj})
 
     def convert(self, value):
         """
