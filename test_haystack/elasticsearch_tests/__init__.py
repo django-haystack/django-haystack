f175f23b8c64c8018063b0732c270931c3246fb2
joaojunior <joaojunior.ma@gmail.com>
2016-06-28 21:01:35 +0000

    Refactoring the code in pull request #1336 . This pull request is to permit use ElasticSearch 2.X

--- test_haystack/elasticsearch_tests/__init__.py
+++ test_haystack/elasticsearch_tests/__init__.py
@@ -10,6 +10,9 @@ warnings.simplefilter('ignore', Warning)
 
 def setup():
     try:
+        import elasticsearch
+        if not ((1, 0, 0) <= elasticsearch.__version__ < (2, 0, 0)):
+            raise ImportError
         from elasticsearch import Elasticsearch, ElasticsearchException
     except ImportError:
         raise unittest.SkipTest("elasticsearch-py not installed.")
@@ -18,5 +21,5 @@ def setup():
     try:
         es.info()
     except ElasticsearchException as e:
-        raise unittest.SkipTest("elasticsearch not running on %r" % settings.HAYSTACK_CONNECTIONS['elasticsearch']['URL'], e)
-
+        raise unittest.SkipTest("elasticsearch not running on %r" % \
+                                settings.HAYSTACK_CONNECTIONS['elasticsearch']['URL'], e)
