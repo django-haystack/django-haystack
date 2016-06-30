89ef54a556e48f4597292eaf23f942284208785a
Chris Adams <chris@improbable.org>
2016-06-30 11:13:46 -0400

    fix: tests will fall back to the Apache archive server
    The Apache 4.10.4 release was quietly removed from the mirrors without a
    redirect. Until we have time to add newer Solr releases to the test
    suite we'll download from the archive and let the Travis build cache
    store it.

--- test_haystack/solr_tests/server/get-solr-download-url.py
+++ test_haystack/solr_tests/server/get-solr-download-url.py
@@ -3,6 +3,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from itertools import chain
 import sys
 
 import requests
@@ -22,14 +23,31 @@ solr_version = sys.argv[1]
 tarball = 'solr-{0}.tgz'.format(solr_version)
 dist_path = 'lucene/solr/{0}/{1}'.format(solr_version, tarball)
 
-download_url = urljoin('http://archive.apache.org/dist/', dist_path)
-mirror_response = requests.get("http://www.apache.org/dyn/mirrors/mirrors.cgi/%s?asjson=1" % dist_path)
+download_url = urljoin('https://archive.apache.org/dist/', dist_path)
+mirror_response = requests.get("https://www.apache.org/dyn/mirrors/mirrors.cgi/%s?asjson=1" % dist_path)
 
-if mirror_response.ok:
-    mirror_data = mirror_response.json()
-    download_url = urljoin(mirror_data['preferred'], mirror_data['path_info'])
+if not mirror_response.ok:
+    print('Apache mirror request returned HTTP %d' % mirror_response.status_code, file=sys.stderr)
+    sys.exit(1)
+
+mirror_data = mirror_response.json()
+
+# Since the Apache mirrors are often unreliable and releases may disappear without notice we'll
+# try the preferred mirror, all of the alternates and backups, and fall back to the main Apache
+# archive server:
+for base_url in chain((mirror_data['preferred'], ), mirror_data['http'], mirror_data['backup'],
+                      ('https://archive.apache.org/dist/', )):
+    test_url = urljoin(base_url, mirror_data['path_info'])
+
     # The Apache mirror script's response format has recently changed to exclude the actual file paths:
-    if not download_url.endswith(tarball):
-        download_url = urljoin(download_url, dist_path)
+    if not test_url.endswith(tarball):
+        test_url = urljoin(test_url, dist_path)
+
+    if requests.head(test_url, allow_redirects=True).status_code == 200:
+        download_url = test_url
+        break
+else:
+    print('None of the Apache mirrors have %s' % dist_path, file=sys.stderr)
+    sys.exit(1)
 
 print(download_url)
