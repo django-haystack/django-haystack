d34411a8b32bdf06e8c326b350d8204acc525e96
Chris Adams <chris@improbable.org>
2016-07-11 10:15:27 -0400

    v2.5.0

--- haystack/__init__.py
+++ haystack/__init__.py
@@ -11,7 +11,7 @@ from haystack.utils import loading
 
 
 __author__ = 'Daniel Lindsley'
-__version__ = (2, 5, 'dev2')
+__version__ = (2, 5, 0)
 
 default_app_config = 'haystack.apps.HaystackConfig'
 
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name='django-haystack',
-    version='2.5.dev2',
+    version='2.5.0',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
