django-haystack/django-haystack

commit 4e1e2e1c5df1ed1c5432b9d26fcb9dc1abab71f4

Fix concurrency issues when building UnifiedIndex
We were getting this error a lot when under load in a multithreaded wsgi
environment:

    Model '%s' has more than one 'SearchIndex`` handling it.

Turns out the connections in haystack.connections and the UnifiedIndex
instance were stored globally. However there is a race condition in
UnifiedIndex.build() when multiple threads both build() at once,
resulting in the above error.

Best fix is to never share the same engine or UnifiedIndex across
multiple threads. This commit does that.

Closes #959
Closes #615

haystack/management/commands/update_index.py
@@ -55,10 +55,10 @@ def worker(bits):
         if 'sqlite3' not in info['ENGINE']:
             try:
                 close_old_connections()
-                if isinstance(connections._connections, dict):
-                    del(connections._connections[alias])
+                if isinstance(connections.thread_local.connections, dict):
+                    del(connections.thread_local.connections[alias])
                 else:
-                    delattr(connections._connections, alias)
+                    delattr(connections.thread_local.connections, alias)
             except KeyError:
                 pass
 

AUTHORS
@@ -107,3 +107,4 @@ Thanks to
     * Ana Carolina (@anacarolinats) and Steve Bussetti (@sbussetti) for adding the ``fuzzy`` operator to
       SearchQuerySet
     * Tadas Dailyda (@skirsdeda) for various patches
+    * Craig de Stigter (@craigds) for a patch fixing concurrency issues when building UnifiedIndex

haystack/utils/loading.py
@@ -4,8 +4,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import copy
 import inspect
+import threading
 import warnings
-
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.module_loading import module_has_submodule
@@ -92,7 +92,7 @@ def load_router(full_router_path):
 class ConnectionHandler(object):
     def __init__(self, connections_info):
         self.connections_info = connections_info
-        self._connections = {}
+        self.thread_local = threading.local()
         self._index = None
 
     def ensure_defaults(self, alias):
@@ -105,16 +105,20 @@ class ConnectionHandler(object):
             conn['ENGINE'] = 'haystack.backends.simple_backend.SimpleEngine'
 
     def __getitem__(self, key):
-        if key in self._connections:
-            return self._connections[key]
+        if not hasattr(self.thread_local, 'connections'):
+            self.thread_local.connections = {}
+        elif key in self.thread_local.connections:
+            return self.thread_local.connections[key]
 
         self.ensure_defaults(key)
-        self._connections[key] = load_backend(self.connections_info[key]['ENGINE'])(using=key)
-        return self._connections[key]
+        self.thread_local.connections[key] = load_backend(self.connections_info[key]['ENGINE'])(using=key)
+        return self.thread_local.connections[key]
 
     def reload(self, key):
+        if not hasattr(self.thread_local, 'connections'):
+            self.thread_local.connections = {}
         try:
-            del self._connections[key]
+            del self.thread_local.connections[key]
         except KeyError:
             pass
 

test_haystack/test_loading.py
@@ -24,7 +24,6 @@ class ConnectionHandlerTestCase(TestCase):
     def test_init(self):
         ch = loading.ConnectionHandler({})
         self.assertEqual(ch.connections_info, {})
-        self.assertEqual(ch._connections, {})
 
         ch = loading.ConnectionHandler({
             'default': {
@@ -38,7 +37,6 @@ class ConnectionHandlerTestCase(TestCase):
                 'URL': 'http://localhost:9001/solr/test_default',
             },
         })
-        self.assertEqual(ch._connections, {})
 
     @unittest.skipIf(pysolr is False, "pysolr required")
     def test_get_item(self):

commit bc541fe7c6cd85f34388742b68d90bc6f670b304

Fix multiprocessing regression in update_index
4e1e2e1c5df1ed1c5432b9d26fcb9dc1abab71f4 introduced a bug because it
used a property name which exists on haystack.ConnectionHandler but not
the Django ConnectionHandler class it's modeled on. Long-term, we should
rename the Haystack class to something like `SearchConnectionHandler`
to avoid future confusion.

Closes #1449

haystack/management/commands/update_index.py
@@ -42,10 +42,10 @@ def update_worker(args):
         if 'sqlite3' not in info['ENGINE']:
             try:
                 close_old_connections()
-                if isinstance(connections.thread_local.connections, dict):
-                    del connections.thread_local.connections[alias]
+                if isinstance(connections._connections, dict):
+                    del connections._connections[alias]
                 else:
-                    delattr(connections.thread_local.connections, alias)
+                    delattr(connections._connections, alias)
             except KeyError:
                 pass
 