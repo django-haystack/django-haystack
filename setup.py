eaaf3a2d064edd5235cdb09d4fd47818d4eb65d4
Kyle T Stapp <RabidCicada@gmail.com>
2017-04-10 22:49:24 -0400

    Reworked start-solr-test-server to work with modern solr.  Reworked solr spinup to create a default core using predefined config in server/confdir

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 install_requires = [
     'Django>=1.8',
-    'Django<1.11',
+    'Django<1.12',
 ]
 
 tests_require = [
--- test_haystack/solr_tests/server/start-solr-test-server.sh
+++ test_haystack/solr_tests/server/start-solr-test-server.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-SOLR_VERSION=4.10.4
+SOLR_VERSION=6.4.0
+SOLR_DIR=solr-${SOLR_VERSION}
 
 cd $(dirname $0)
 
@@ -24,32 +25,34 @@ if [ ! -f ${SOLR_ARCHIVE} ]; then
     curl -Lo $SOLR_ARCHIVE ${SOLR_DOWNLOAD_URL} || (echo "Unable to download ${SOLR_DOWNLOAD_URL}"; exit 2)
 fi
 
-echo "Extracting Solr ${SOLR_VERSION} to `pwd`/solr4/"
-rm -rf solr4
-mkdir solr4
-tar -C solr4 -xf ${SOLR_ARCHIVE} --strip-components 2 solr-${SOLR_VERSION}/example
-tar -C solr4 -xf ${SOLR_ARCHIVE} --strip-components 1 solr-${SOLR_VERSION}/dist solr-${SOLR_VERSION}/contrib
+echo "Extracting Solr ${SOLR_ARCHIVE} to `pwd`/${SOLR_DIR}"
+rm -rf solr-*
+mkdir ${SOLR_DIR}
+tar -C ${SOLR_DIR} -xf ${SOLR_ARCHIVE} --strip-components=1
+#tar -C solr6 -xf ${SOLR_ARCHIVE} --strip-components 2 solr-${SOLR_VERSION}/example
+#tar -C solr6 -xf ${SOLR_ARCHIVE} --strip-components 1 solr-${SOLR_VERSION}/dist solr-${SOLR_VERSION}/contrib
 
-echo "Changing into solr4"
+echo "Changing into ${SOLR_DIR} "
 
-cd solr4
+cd ${SOLR_DIR}
 
-echo "Configuring Solr"
-
-cp ${TEST_ROOT}/solrconfig.xml solr/collection1/conf/solrconfig.xml
-cp ${TEST_ROOT}/schema.xml solr/collection1/conf/schema.xml
+echo "Creating Solr Core"
+GC_LOG_OPTS= ./bin/solr start
+GC_LOG_OPTS= ./bin/solr create -c collection1 -d ../confdir
+GC_LOG_OPTS= ./bin/solr stop
 
 # Fix paths for the content extraction handler:
-perl -p -i -e 's|<lib dir="../../../contrib/|<lib dir="../../contrib/|'g solr/*/conf/solrconfig.xml
-perl -p -i -e 's|<lib dir="../../../dist/|<lib dir="../../dist/|'g solr/*/conf/solrconfig.xml
+#perl -p -i -e 's|<lib dir="../../../contrib/|<lib dir="../../contrib/|'g solr/*/conf/solrconfig.xml
+#perl -p -i -e 's|<lib dir="../../../dist/|<lib dir="../../dist/|'g solr/*/conf/solrconfig.xml
 
 # Add MoreLikeThis handler
-perl -p -i -e 's|<!-- A Robust Example|<!-- More like this request handler -->\n  <requestHandler name="/mlt" class="solr.MoreLikeThisHandler" />\n\n\n  <!-- A Robust Example|'g solr/*/conf/solrconfig.xml
+#perl -p -i -e 's|<!-- A Robust Example|<!-- More like this request handler -->\n  <requestHandler name="/mlt" class="solr.MoreLikeThisHandler" />\n\n\n  <!-- A Robust Example|'g solr/*/conf/solrconfig.xml
 
 echo 'Starting server'
+cd server
 # We use exec to allow process monitors to correctly kill the
 # actual Java process rather than this launcher script:
-export CMD="java -Djetty.port=9001 -Djava.awt.headless=true -Dapple.awt.UIElement=true -jar start.jar"
+export CMD="java -Djetty.port=9001 -Djava.awt.headless=true -Dapple.awt.UIElement=true -jar start.jar --module=http"
 
 if [ -z "${BACKGROUND_SOLR}" ]; then
     exec $CMD
