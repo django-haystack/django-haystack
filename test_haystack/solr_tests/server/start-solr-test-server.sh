d6648289be809935ad3e0a4460fb26cb09d080b0
Kyle T Stapp <RabidCicada@gmail.com>
2017-04-12 17:36:35 -0400

    Fixed LiveSolrContentExtractionTestCase Failure.  Reworked core creation and configuration a little

--- test_haystack/solr_tests/server/start-solr-test-server.sh
+++ test_haystack/solr_tests/server/start-solr-test-server.sh
@@ -5,6 +5,8 @@ set -e
 SOLR_VERSION=6.4.0
 SOLR_DIR=solr-${SOLR_VERSION}
 
+SOLR_PORT=9001
+
 cd $(dirname $0)
 
 export TEST_ROOT=$(pwd)
@@ -28,22 +30,17 @@ fi
 echo "Extracting Solr ${SOLR_ARCHIVE} to `pwd`/${SOLR_DIR}"
 rm -rf solr-*
 mkdir ${SOLR_DIR}
+FULL_SOLR_DIR=$(readlink -f ./${SOLR_DIR})
 tar -C ${SOLR_DIR} -xf ${SOLR_ARCHIVE} --strip-components=1
-#tar -C solr6 -xf ${SOLR_ARCHIVE} --strip-components 2 solr-${SOLR_VERSION}/example
-#tar -C solr6 -xf ${SOLR_ARCHIVE} --strip-components 1 solr-${SOLR_VERSION}/dist solr-${SOLR_VERSION}/contrib
 
 echo "Changing into ${SOLR_DIR} "
 
 cd ${SOLR_DIR}
 
 echo "Creating Solr Core"
-GC_LOG_OPTS= ./bin/solr start
-GC_LOG_OPTS= ./bin/solr create -c collection1 -d ../confdir
-GC_LOG_OPTS= ./bin/solr stop
-
-# Fix paths for the content extraction handler:
-#perl -p -i -e 's|<lib dir="../../../contrib/|<lib dir="../../contrib/|'g solr/*/conf/solrconfig.xml
-#perl -p -i -e 's|<lib dir="../../../dist/|<lib dir="../../dist/|'g solr/*/conf/solrconfig.xml
+GC_LOG_OPTS= ./bin/solr start -p ${SOLR_PORT}
+GC_LOG_OPTS= ./bin/solr create -c collection1 -d ../confdir -p ${SOLR_PORT}
+GC_LOG_OPTS= ./bin/solr stop -p ${SOLR_PORT}
 
 # Add MoreLikeThis handler
 #perl -p -i -e 's|<!-- A Robust Example|<!-- More like this request handler -->\n  <requestHandler name="/mlt" class="solr.MoreLikeThisHandler" />\n\n\n  <!-- A Robust Example|'g solr/*/conf/solrconfig.xml
@@ -52,7 +49,7 @@ echo 'Starting server'
 cd server
 # We use exec to allow process monitors to correctly kill the
 # actual Java process rather than this launcher script:
-export CMD="java -Djetty.port=9001 -Djava.awt.headless=true -Dapple.awt.UIElement=true -jar start.jar --module=http"
+export CMD="java -Djetty.port=${SOLR_PORT} -Djava.awt.headless=true -Dapple.awt.UIElement=true -jar start.jar --module=http -Dsolr.install.dir=${FULL_SOLR_DIR}"
 
 if [ -z "${BACKGROUND_SOLR}" ]; then
     exec $CMD
