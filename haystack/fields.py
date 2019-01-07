c86893c08abb44b7f6602f34a1f07cb8cba262e6
Martin Pauly <m.pauly@jumart.de>
2019-01-07 19:20:12 +0100

    Move import into function to avoid GDAL as a global dependency

--- haystack/fields.py
+++ haystack/fields.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import re
 from inspect import ismethod
 
-from django.contrib.gis.geos import Point
 from django.template import loader
 from django.utils import datetime_safe, six
 
@@ -260,6 +259,7 @@ class LocationField(SearchField):
         return "%s,%s" % (pnt_lat, pnt_lng)
 
     def convert(self, value):
+        from django.contrib.gis.geos import Point
         from haystack.utils.geo import ensure_point
 
         if value is None:
