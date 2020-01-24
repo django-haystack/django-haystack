1e8ac2035dfb66107070edc4eaa12376795981ff
Alexander Clausen <alex@gc-web.de>
2020-01-24 23:44:45 +0100

    add linebreaks to make docker command readable

--- test_haystack/solr_tests/server/start-solr-test-server.sh
+++ test_haystack/solr_tests/server/start-solr-test-server.sh
@@ -13,4 +13,7 @@ fi
 # https://stackoverflow.com/a/246128/540644
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker run ${ARGS} -p 9001:8983 -v $DIR/solr-setup.sh:/solr-setup.sh -v $DIR/confdir:/confdir:ro solr:${SOLR_VERSION}-slim bash -c "/solr-setup.sh"
+docker run --rm ${ARGS} -p 9001:8983 \
+    -v $DIR/solr-setup.sh:/solr-setup.sh \
+    -v $DIR/confdir:/confdir:ro \
+    --name haystack_solr solr:${SOLR_VERSION}-slim bash -c "/solr-setup.sh"
