90d35b0c86fb6ea4ea1904b1df1124b594a6f226
Dulmandakh <dulmandakh@gmail.com>
2021-04-14 08:08:52 +0800

    publish

--- setup.py
+++ setup.py
@@ -3,14 +3,6 @@
 
 from setuptools import setup
 
-try:
-    from setuptools import setup
-except ImportError:
-    from ez_setup import use_setuptools
-
-    use_setuptools()
-    from setuptools import setup
-
 install_requires = ["Django>=2.2"]
 
 tests_require = [
@@ -47,7 +39,8 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
@@ -57,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Utilities",
     ],
     zip_safe=False,
