69f2b0914de49910a6747f7d69fbf85a9e590f00
Christian Clauss <cclauss@me.com>
2023-08-01 12:08:52 +0200

    # noqa: F401

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -4,7 +4,7 @@ import re
 from django.conf import settings
 
 from haystack.constants import DJANGO_CT, DJANGO_ID, ID
-from haystack.utils.highlighting import Highlighter  # noqa=F401
+from haystack.utils.highlighting import Highlighter  # noqa: F401
 
 IDENTIFIER_REGEX = re.compile(r"^[\w\d_]+\.[\w\d_]+\.[\w\d-]+$")
 
